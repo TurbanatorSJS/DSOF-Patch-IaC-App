8f37d39f25c648153ea43b6f75d08b29abd8b15a
SimranJeet Singh <turbanatorsjs@gmail.com>
2023-11-23 15:56:04 +0530

    Update main.tf

--- modules/storage/main.tf
+++ modules/storage/main.tf
@@ -1,21 +1,28 @@
 resource "aws_s3_bucket" "insecure-bucket" {
   bucket = "insecure-bucket"
-  versioning.enabled = true
-  logging = true
-  versioning.mfa_delete = true
+  acl    = "private"  # Set ACL to private
+  versioning {
+    enabled = true
+  }
+  logging {
+    target_bucket = aws_s3_bucket.insecure-bucket.bucket
+    target_prefix = "s3-logs/"
+  }
+  force_destroy = true  # Enable force_destroy to allow deletion of non-empty bucket
 }
 
-# resource "aws_s3_bucket_public_access_block" "insecure-bucket" {
-#   block_public_acls       = true
-#   block_public_policy     = true
-#   ignore_public_acls      = true
-#   restrict_public_buckets = true
-# }
+resource "aws_s3_bucket_public_access_block" "insecure-bucket" {
+  bucket = aws_s3_bucket.insecure-bucket.bucket
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
 
 resource "aws_ebs_volume" "example" {
   availability_zone = "us-east-1a"
   size              = 20
-  encrypted = true
+  encrypted         = true
   tags = {
     Name = "insecure"
   }
